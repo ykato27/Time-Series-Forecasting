15df87e95f9e168b506eca2d26edcd9db28dc66b
yuki-kato27 <yuki_kato@albert2005.co.jp>
2021-05-22 13:45:32 +0900

    test

---
+++ hoge.py
@@ -0,0 +1,21 @@
+def hoge():
+    if 1 == 1:
+        print('hoge')
+    elif 1 == 2:
+        pass
+    elif 1 == 3:
+        pass
+    elif 1 == 4:
+        pass
+    elif 1 == 5:
+        pass
+    elif 1 == 6:
+        pass
+    elif 1 == 7:
+        pass
+    elif 1 == 8:
+        pass
+    elif 1 == 9:
+        pass
+    elif 1 == 10:
+        pass
